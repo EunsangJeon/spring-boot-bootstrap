c0a473ce0625daa63cafea5187102a9fb12e282f
Eunsang <nicolaus1207@gmail.com>
2020-05-06 23:29:16 +0900

    auto configuration

--- basic/src/main/java/com/nick/springboot/basic/BasicApplication.java
+++ basic/src/main/java/com/nick/springboot/basic/BasicApplication.java
@@ -2,12 +2,19 @@ package com.nick.springboot.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
+
+		ApplicationContext applicationContext =
+				SpringApplication.run(BasicApplication.class, args);
+
+		for (String name: applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
---
+++ basic/src/main/java/com/nick/springboot/basic/BooksController.java
@@ -0,0 +1,18 @@
+package com.nick.springboot.basic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(
+                new Book(1L, "Game of Thrones", "George R. R. Martin")
+        );
+    }
+}
